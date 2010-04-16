b419795f6fa7189a45356f8c04c47ad288a0f331
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-16 07:07:35 +0000

    First upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpdisplay@32259 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ trunk/ext_localconf.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+ 	die ('Access denied.');
+}
+t3lib_extMgm::addUserTSConfig('
+	options.saveDocNew.tx_phpdisplay_displays=1
+');
+
+// Register method with generic BE ajax calls handler
+// (as from TYPO3 4.2)
+
+$TYPO3_CONF_VARS['BE']['AJAX']['phpdisplay::saveConfiguration'] = 'typo3conf/ext/phpdisplay/class.tx_temlatedisplay_ajax.php:tx_phpdisplay_ajax->saveConfiguration';
+$TYPO3_CONF_VARS['BE']['AJAX']['phpdisplay::saveTemplate'] = 'typo3conf/ext/phpdisplay/class.tx_temlatedisplay_ajax.php:tx_phpdisplay_ajax->saveTemplate';
+/*
+ * Hook for loading Javascript and CSS in the backend
+ */
+#if (TYPO3_MODE == 'BE')	{
+#	require_once(t3lib_extMgm::extPath('phpdisplay').'hook/user_addBackendLibrary.php');
+#}
+#$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/template.php']['preStartPageHook'][] = 'user_addBackendLibrary';
+
+// Register as Data Consumer service
+// Note that the subtype corresponds to the name of the database table
+t3lib_extMgm::addService($_EXTKEY,  'dataconsumer' /* sv type */,  'tx_phpdisplay_dataconsumer' /* sv key */,
+	array(
+
+		'title' => 'Data Display Engine',
+		'description' => 'Generic Data Consumer for recordset-type data structures',
+
+		'subtype' => 'tx_phpdisplay_displays',
+
+		'available' => TRUE,
+		'priority' => 50,
+		'quality' => 50,
+
+		'os' => '',
+		'exec' => '',
+
+		'classFile' => t3lib_extMgm::extPath($_EXTKEY, 'class.tx_phpdisplay.php'),
+		'className' => 'tx_phpdisplay',
+	)
+);
+?>
