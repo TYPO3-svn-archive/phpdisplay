8cac9a3435c082090dc4e83e8a63670cd189b4d7
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-27 13:21:31 +0000

    phpdisplay: released on TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpdisplay@38656 735d13b6-9817-0410-8766-e36946ffe9aa

--- trunk/ext_emconf.php
+++ trunk/ext_emconf.php
@@ -1,23 +1,23 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "phpdisplay"
+# Extension Manager/Repository config file for ext "phpdisplay".
 #
-# Auto generated 23-07-2008 13:29
+# Auto generated 27-09-2010 15:11
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Plain PHP based Data Display (Data Consumer)',
-	'description' => 'Use Plain PHP templates to display any kind of data returned by a Data Provider',
+	'title' => 'Plain PHP based Data Display (Data Consumer) - Tesseract project',
+	'description' => 'Use Plain PHP templates to display any kind of data returned by a Data Provider. More info on http://www.typo3-tesseract.com',
 	'category' => 'fe',
 	'author' => 'Fabien Udriot',
 	'author_email' => 'fabien.udriot@ecodev.ch',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'tesseract',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
@@ -29,17 +29,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.9',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'tesseract' => '0.1.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
+			'tesseract' => '1.0.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"8077";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d43a";s:14:"ext_tables.php";s:4:"4be3";s:14:"ext_tables.sql";s:4:"14fc";s:36:"icon_tx_phpdisplay_displays.gif";s:4:"475a";s:16:"Resources/Private/Language/locallang_db.xml";s:4:"b5c6";s:7:"tca.php";s:4:"ebc7";s:19:"doc/wizard_form.dat";s:4:"b285";s:20:"doc/wizard_form.html";s:4:"25bc";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"7f63";s:10:"README.txt";s:4:"b948";s:23:"class.tx_phpdisplay.php";s:4:"4363";s:32:"class.tx_phpdisplay_template.php";s:4:"cb80";s:16:"ext_autoload.php";s:4:"cb29";s:12:"ext_icon.gif";s:4:"9530";s:12:"ext_icon.png";s:4:"55ad";s:17:"ext_localconf.php";s:4:"fa6b";s:14:"ext_tables.php";s:4:"ad1b";s:14:"ext_tables.sql";s:4:"b2b4";s:44:"Configuration/TCA/tx_phpdisplay_displays.php";s:4:"dd82";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"683d";s:50:"Resources/Public/images/tx_phpdisplay_displays.png";s:4:"55ad";s:14:"doc/manual.pdf";s:4:"aa8d";s:14:"doc/manual.sxw";s:4:"9740";s:17:"samples/dummy.php";s:4:"c236";}',
+	'suggests' => array(
+	),
 );
 
 ?>
