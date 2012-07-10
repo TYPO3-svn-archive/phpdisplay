fc3bdb11f7751a5beafe814092eb065b2337393e
fab1en <fab1en@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-10 09:53:46 +0000

    Update version number in ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpdisplay/trunk@64345 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "phpdisplay".
 #
-# Auto generated 27-09-2010 15:11
+# Auto generated 10-07-2012 11:53
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.1.0',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-4.7.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"7f63";s:10:"README.txt";s:4:"b948";s:23:"class.tx_phpdisplay.php";s:4:"4363";s:32:"class.tx_phpdisplay_template.php";s:4:"cb80";s:16:"ext_autoload.php";s:4:"cb29";s:12:"ext_icon.gif";s:4:"9530";s:12:"ext_icon.png";s:4:"55ad";s:17:"ext_localconf.php";s:4:"fa6b";s:14:"ext_tables.php";s:4:"ad1b";s:14:"ext_tables.sql";s:4:"b2b4";s:44:"Configuration/TCA/tx_phpdisplay_displays.php";s:4:"dd82";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"683d";s:50:"Resources/Public/images/tx_phpdisplay_displays.png";s:4:"55ad";s:14:"doc/manual.pdf";s:4:"aa8d";s:14:"doc/manual.sxw";s:4:"9740";s:17:"samples/dummy.php";s:4:"c236";}',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"3b70";s:10:"README.txt";s:4:"b948";s:23:"class.tx_phpdisplay.php";s:4:"e02b";s:32:"class.tx_phpdisplay_template.php";s:4:"1b68";s:16:"ext_autoload.php";s:4:"cb29";s:12:"ext_icon.gif";s:4:"9530";s:12:"ext_icon.png";s:4:"55ad";s:17:"ext_localconf.php";s:4:"fa6b";s:14:"ext_tables.php";s:4:"df7a";s:14:"ext_tables.sql";s:4:"b2b4";s:44:"Configuration/TCA/tx_phpdisplay_displays.php";s:4:"adef";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"683d";s:50:"Resources/Public/images/tx_phpdisplay_displays.png";s:4:"55ad";s:14:"doc/manual.pdf";s:4:"f117";s:14:"doc/manual.sxw";s:4:"f7bf";s:17:"samples/dummy.php";s:4:"55a6";}',
 	'suggests' => array(
 	),
 );
